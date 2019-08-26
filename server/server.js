bc30e8b6a31d51031bfde91fdc338aceba42206d
Luis <luisparracamara@gmail.com>
2019-08-25 19:27:01 -0700

    rest server

---
+++ server/server.js
@@ -0,0 +1,45 @@
+require("./config/config.js");
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+ 
+app.get('/', function (req, res) {
+  res.json('Hello World')
+})
+
+app.get("/usuario", (req,res) => {
+    
+    res.json('get usuario');
+});
+
+app.post("/usuario", (req, res) => {
+    let body = req.body;
+    res.json({
+        persona: body
+    });
+});
+
+app.put("/usuario/:id", (req, res) => {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete("/usuario", (req, res) => {
+    res.json('delete usuario');
+});
+ 
+app.listen(process.env.PORT, ()=> {
+    console.log("escuchando en puerto ",3000);
+    
+})
+
